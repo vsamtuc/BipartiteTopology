65bd31f2fe63a9475e96b288b3fff4950b2423bd
Vasilis Samoladas <vsam@softnet.tuc.gr>
2023-05-02 13:49:43 +0300

    Set-up testing

---
+++ src/test/java/BipartiteTopologyAPI/TestingTests.java
@@ -0,0 +1,24 @@
+package BipartiteTopologyAPI;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class TestingTests {
+
+    @Test
+    void failingtest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void testTesting() {
+
+        System.out.println("Hello world");
+
+
+    }
+
+
+}
